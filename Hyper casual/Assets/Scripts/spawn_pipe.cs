660e0a7eb4822ec0e0bf98ddd8cb010b9e3ea086
karthikeya-33 <152944134+karthikeya-33@users.noreply.github.com>
2024-01-28 22:07:19 +0530

    error

--- Hyper casual/Assets/Scripts/spawn_pipe.cs
+++ Hyper casual/Assets/Scripts/spawn_pipe.cs
@@ -14,6 +14,12 @@ public class spawn_pipe : MonoBehaviour
             Instantiate(Pipeprefab, new Vector3(0,10,0), Quaternion.identity);
            //Time.timeScale = 0f;
           // Debug.Log("spawn");
+
+        }
+          else if (other.gameObject.tag == "pipe")
+        {
+          Destroy(other.gameObject, 30f);
+          Debug.Log("err");
         }
     }
 }
