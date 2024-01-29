eae36526945e82d03c5b12aa54e294d1775c4835
Kabir1430 <104447995+Kabir1430@users.noreply.github.com>
2024-01-29 14:58:54 +0530

    k

--- Hyper casual/Assets/fps.cs
+++ Hyper casual/Assets/fps.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class fps : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TMP_Text fpsText;
 
-    // Update is called once per frame
+    float deltaTime = 0.0f;
+
     void Update()
     {
-        
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+
+        // Update the TextMeshPro text with the current FPS
+        if (fpsText != null)
+        {
+            float msec = deltaTime * 1000.0f;
+            float fps = 1.0f / deltaTime;
+            string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+            fpsText.text = text;
+        }
     }
 }
